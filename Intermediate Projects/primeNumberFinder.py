a892ed4d0d005aeaedeb40f2f0bf91a445cdb9a9
Malia Fairbanks <Malia.Fairbanks7@gmail.com>
2021-11-28 10:25:26 -0600

    Update primeNumberFinder.py

--- Intermediate Projects/primeNumberFinder.py
+++ Intermediate Projects/primeNumberFinder.py
@@ -1,8 +1,8 @@
 def prime_finder(n):
-    out = list()
-    out.append(2)
+    myList = list()
+    myList.append(2)
     for num in range(3, n+1, 2):
         if all(num % i != 0 for i in range(2, int(num**.5 ) + 1)):
-            out.append(num)
+            myList.append(num)
     print (out)
 prime_finder(11)
